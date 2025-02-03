04535e31f3091e3565c8293f90a3fcf1fd07089d
ilkhoeri <khoeriilham@gmail.com>
2025-02-03 23:04:23 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,25 +4,25 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // reactStrictMode: true,
   // experimental: { appDir: true },
-  output: "export", // Enable static export
+  // output: "export", // Enable static export
   trailingSlash: true, // Recommended for GitHub Pages
   // If you have dynamic routes, you might need to specify them
-  exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
-    return {
-      "/": { page: "/" },
-      "/ocx": { page: "/ocx" },
-      "/cvx": { page: "/cvx" },
-      "/cnx": { page: "/cnx" },
-      "/clean": { page: "/clean" },
-      "/links": { page: "/links" },
-      "/license": { page: "/license" },
-      "/changelog": { page: "/changelog" },
-      "/coc": { page: "/coc" },
-      "/others": { page: "/others" },
-      "/about": { page: "/about" },
-      "/about/app": { page: "/about/app" }
-    };
-  }
+  // exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+  //   return {
+  //     "/": { page: "/" },
+  //     "/ocx": { page: "/ocx" },
+  //     "/cvx": { page: "/cvx" },
+  //     "/cnx": { page: "/cnx" },
+  //     "/clean": { page: "/clean" },
+  //     "/links": { page: "/links" },
+  //     "/license": { page: "/license" },
+  //     "/changelog": { page: "/changelog" },
+  //     "/coc": { page: "/coc" },
+  //     "/others": { page: "/others" },
+  //     "/about": { page: "/about" },
+  //     "/about/app": { page: "/about/app" }
+  //   };
+  // }
 };
 
 export default withContentlayer(nextConfig);
