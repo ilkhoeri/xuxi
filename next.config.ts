4be15c605a8423123664c218b498c942a710028c
ilkhoeri <khoeriilham@gmail.com>
2025-02-03 22:04:41 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,19 +7,19 @@ const nextConfig: NextConfig = {
   // experimental: {
   //   // appDir: true,
   // },
-  output: "export", // Enable static export
-  trailingSlash: true, // Recommended for GitHub Pages
+  // output: "export", // Enable static export
+  trailingSlash: true // Recommended for GitHub Pages
   // If you have dynamic routes, you might need to specify them
-  exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
-    // return {
-    //   "/": { page: "/" },
-    //   "/ocx": { page: "/ocx" },
-    //   // dan seterusnya ...
-    //   "/about": { page: "/about" },
-    //   "/about/app": { page: "/about/app" }
-    // };
-    return generateRoutes(ROUTES["docs"]);
-  }
+  // exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+  //   // return {
+  //   //   "/": { page: "/" },
+  //   //   "/ocx": { page: "/ocx" },
+  //   //   // dan seterusnya ...
+  //   //   "/about": { page: "/about" },
+  //   //   "/about/app": { page: "/about/app" }
+  //   // };
+  //   return generateRoutes(ROUTES["docs"]);
+  // }
 };
 
 export default withContentlayer(nextConfig);
