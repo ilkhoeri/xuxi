1d9f5b3ed945154d15c3e5be4308e53f4bab336d
ilkhoeri <khoeriilham@gmail.com>
2025-02-03 22:26:23 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,26 @@
 import { withContentlayer } from "next-contentlayer2";
 import type { NextConfig } from "next";
-import { generateRoutes, ROUTES } from "./routes";
 
 const nextConfig: NextConfig = {
   // reactStrictMode: true,
-  // experimental: {
-  //   // appDir: true,
-  // },
+  // experimental: { appDir: true },
   output: "export", // Enable static export
   trailingSlash: true, // Recommended for GitHub Pages
   // If you have dynamic routes, you might need to specify them
   exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
-    // return {
-    //   "/": { page: "/" },
-    //   "/ocx": { page: "/ocx" },
-    //   // dan seterusnya ...
-    //   "/about": { page: "/about" },
-    //   "/about/app": { page: "/about/app" }
-    // };
-    return generateRoutes(ROUTES["docs"]);
+    return {
+      "/": { page: "/" },
+      "/ocx": { page: "/ocx" },
+      "/cvx": { page: "/cvx" },
+      "/cnx": { page: "/cnx" },
+      "/clean": { page: "/clean" },
+      "/links": { page: "/links" },
+      "/license": { page: "/license" },
+      "/coc": { page: "/coc" },
+      "/others": { page: "/others" },
+      "/about": { page: "/about" },
+      "/about/app": { page: "/about/app" }
+    };
   }
 };
 
